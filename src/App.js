b8df3f6bc93cb158b445d7e01e97f3c0242b0f3f
srinivas.bh <srinivas.bh@agilophiles.com>
2024-03-07 12:58:29 +0530

    Added the Routing to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import MainContainer from './components/MainContainer';
+import WatchPage from './components/WatchPage';
+
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />
+      },
+      {
+        path: "/watch/:id",
+        element: <WatchPage />
+      }
+    ]
+  }
+])
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <RouterProvider router={appRouter}/>
     </div>
   );
 }
