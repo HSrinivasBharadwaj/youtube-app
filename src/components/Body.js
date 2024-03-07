ebc1483a5c1665e7180ebe396b51d004984f461e
srinivas.bh <srinivas.bh@agilophiles.com>
2024-03-07 12:58:45 +0530

    Added the outlet to the Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import SideBar from './SideBar';
-import MainContainer from './MainContainer';
+import { Outlet } from 'react-router-dom';
 
 export default function Body() {
   return (
     <main className='flex justify-between'>
      <SideBar />
-     <MainContainer />
+     <Outlet />
     </main>
   )
 }
