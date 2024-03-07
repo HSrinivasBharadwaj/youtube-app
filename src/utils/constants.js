046d548d3bd742a30d7907f4984d9f767ed8fef8
srinivas.bh <srinivas.bh@agilophiles.com>
2024-03-07 13:01:32 +0530

    Added the url for the individual youtube video details based on the id

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRviA0jJ6s689bkG3RqUe6kxLGaGmEMdzj1X74QPLd3lhFEcJugo8sUPXOyaPqAqESiBTM&usqp=CAU"
 export const YOUTUBE_API = " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=AIzaSyDNUK7nvK-iSW_ttu4LGSsFbIInPeYKPoo"
+export const YOUTUBE_WATCH_PAGE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=AIzaSyDNUK7nvK-iSW_ttu4LGSsFbIInPeYKPoo&id="
