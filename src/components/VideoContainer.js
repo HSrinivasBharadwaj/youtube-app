c61c99498538439b6f22372d77f002e48a15cd35
srinivas.bh <srinivas.bh@agilophiles.com>
2024-03-07 11:49:15 +0530

    Added the shimmer to the loading state

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import useFetchVideoData from '../hooks/useFetchVideoData';
 import VideoCard from './VideoCard';
+import Shimmer from './Shimmer';
 
 export default function VideoContainer() {
   const {loading,error,popularVideos} = useFetchVideoData();
   if (loading || !popularVideos) {
-    return <div>Loading...</div>
+    return <Shimmer />
   }
   if (error) {
     return <p className='text-red-500'>{error}</p>
