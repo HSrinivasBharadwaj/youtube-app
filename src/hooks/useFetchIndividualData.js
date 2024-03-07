716251e8c24a2b635c68e3a0015b2174e8a2bd89
srinivas.bh <srinivas.bh@agilophiles.com>
2024-03-07 13:01:02 +0530

    CustomHook for fetching the individual video details

---
+++ src/hooks/useFetchIndividualData.js
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { YOUTUBE_WATCH_PAGE_API } from '../utils/constants';
+
+export default function useFetchIndividualData(id) {
+    const [loading, setLoading] = useState(false);
+    const [individualVideoData, setIndividualVideoData] = useState([]);
+    const [error, setError] = useState("");
+    const fetchIndividualData = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(YOUTUBE_WATCH_PAGE_API+id);
+            setIndividualVideoData(response.data.items);
+            setLoading(false);
+        } catch (error) {
+            setError(error);
+            setLoading(false)
+        }
+    }
+    useEffect(() => {
+        fetchIndividualData()
+    },[])
+  return {error,individualVideoData,error}
+}
