1fe306cb3d5de8c5ecaf3e4242edaafe6369ae32
srinivas.bh <srinivas.bh@agilophiles.com>
2024-03-14 15:12:13 +0530

    Added the search and chat Reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import toggleMenuReducer from '../features/toggleMenu/toggleMenuSlice';
-import searchReducer from '../features/toggleMenu/searchSlice';;
+import searchReducer from '../features/toggleMenu/searchSlice';
+import chatReducer from '../features/toggleMenu/chatSlice';
 
 const store = configureStore({
     reducer: {
         toggleMenu: toggleMenuReducer,
-        search: searchReducer
+        search: searchReducer,
+        chat: chatReducer
     }
 })
 
