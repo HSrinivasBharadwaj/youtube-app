9820716e05c92841543e071974be69ef1e1b8764
srinivas.bh <srinivas.bh@agilophiles.com>
2024-03-14 10:07:42 +0530

    Rendering the comments Container component in the watchpage

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import useFetchIndividualData from '../hooks/useFetchIndividualData';
 import { useDispatch } from 'react-redux';
 import { closeSideBar } from '../features/toggleMenu/toggleMenuSlice';
+import CommentsContainer from './CommentsContainer';
+
 
 export default function WatchPage() {
     const params = useParams();
@@ -19,20 +21,24 @@ export default function WatchPage() {
     }
 
     return (
-        <div className='m-10 w-3/4'>
-            {individualVideoData.map((videoDetails) => (
-                <div key={videoDetails.id}>
-                    <iframe width="1150" height="515" src={`https://www.youtube.com/embed/${videoDetails.id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className='rounded-2xl cursor-pointer' allowfullscreen></iframe>
-                    <h1 className='text-xl font-bold mt-3'>{videoDetails.snippet.title}</h1>
-                    <div className='flex items-center mt-3'>
-                        <p className='text-gray-600'>{videoDetails.snippet.channelTitle}</p>
-                        <button className='bg-black text-white rounded-full cursor-pointer p-2 ml-4'>Subscribe</button>
+        <div>
+            <div className='m-10 w-3/4'>
+                {individualVideoData.map((videoDetails) => (
+                    <div key={videoDetails.id}>
+                        <iframe width="1150" height="515" src={`https://www.youtube.com/embed/${videoDetails.id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className='rounded-2xl cursor-pointer' allowfullscreen></iframe>
+                        <h1 className='text-xl font-bold mt-3'>{videoDetails.snippet.title}</h1>
+                        <div className='flex items-center mt-3'>
+                            <p className='text-gray-600'>{videoDetails.snippet.channelTitle}</p>
+                            <button className='bg-black text-white rounded-full cursor-pointer p-2 ml-4'>Subscribe</button>
+                        </div>
+                        <div className='bg-gray-200 text-gray-800 p-5 rounded-md mt-3'>
+                            <p>{videoDetails.snippet.description}</p>
+                        </div>
                     </div>
-                    <div className='bg-gray-200 text-gray-800 p-5 rounded-md mt-3'>
-                        <p>{videoDetails.snippet.description}</p>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
+            <CommentsContainer />
         </div>
+
     );
 }
